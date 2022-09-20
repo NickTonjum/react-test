622a99053bdd231401a3de4032f9e2acf111659c
Nicolas Tonjum <nicktonjum16@gmail.com>
2022-09-20 08:18:06 -0600

    feat(chart): added chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import ReactECharts from 'echarts-for-react';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,12 +13,29 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <ReactECharts option={{
+          title: {
+            text: 'ECharts Getting Started Example'
+          },
+          tooltip: {},
+          xAxis: {
+            data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+          },
+          yAxis: {},
+          series: [
+            {
+              name: 'sales',
+              type: 'bar',
+              data: [5, 20, 36, 10, 10, 20]
+            }
+          ]
+        }} />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
     </div>
   )
-}
+};
 
-export default App
+export default App;
