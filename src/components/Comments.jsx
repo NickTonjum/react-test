45a9fb7c957bd16200f797b72ce62eb1c3782278
Nicolas Tonjum <nicktonjum16@gmail.com>
2022-09-21 22:14:49 -0600

    feat(comments): added loading spinner for data

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,18 +2,46 @@ import { useState } from 'react';
 import axios from 'axios';
 import React from 'react';
 import '../App.css';
+import { Audio } from 'react-loader-spinner';
 
 class Comments extends React.Component {
   state = {
-    persons: []
+    persons: [],
+    loaded: false
   }
 
   componentDidMount() {
     axios.get(`https://jsonplaceholder.typicode.com/comments`)
       .then(res => {
         const persons = res.data;
-        this.setState({ persons });
+        this.setState({ persons, loaded: true });
       })
+  }
+
+  getData() {
+    if (this.state.loaded) {
+      return <ul class="frow column-start">
+        {
+          this.state.persons
+            .map(person =>
+              <li key={person.id} class="frow column-start">
+                <div><span class="weight-bold">Name:</span> {person.name}</div>
+                <div><span class="weight-bold">Email:</span> {person.email}</div>
+                <div><span class="weight-bold">Body:</span> {person.body}</div>
+              </li>
+            )
+        }
+      </ul>;
+    }
+    else {
+      return  <div class="frow centered mt-20"><Audio
+        height="50"
+        width="50"
+        radius="9"
+        color="white"
+        ariaLabel="loading"
+      /></div>;
+    }
   }
 
   render() {
@@ -21,18 +49,7 @@ class Comments extends React.Component {
       <div>
         <h3>Hello this is the Comments page</h3>
         <h4>The following data is pulled from `https://jsonplaceholder.typicode.com/comments` using axios GET</h4>
-        <ul class="frow column-start">
-          {
-            this.state.persons
-              .map(person =>
-                <li key={person.id} class="frow column-start">
-                  <div><span class="weight-bold">Name:</span> {person.name}</div>
-                  <div><span class="weight-bold">Email:</span> {person.email}</div>
-                  <div><span class="weight-bold">Body:</span> {person.body}</div>
-                </li>
-              )
-          }
-        </ul>
+        <div>{this.getData()}</div>
       </div>
     )
   }
